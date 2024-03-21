5303f2b84120a3eed4884f15db747c07399ece98
Berk <brk111880@gmail.com>
2024-03-22 00:29:48 +0300

    son
    son

--- Foniste/FonApi/obj/Release/net6.0/FonApi.AssemblyInfo.cs
+++ Foniste/FonApi/obj/Release/net6.0/FonApi.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6742267136d7ee7dab8332c3d186484b80ea4cb2")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881e189e83069dcb1550b11fda80080f41553fb4")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("FonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
