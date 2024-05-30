cafa3946cc8734ff209dd51a3f822ac016bda7d1
Berk <brk111880@gmail.com>
2024-05-30 15:09:27 +0300

    Proje ekleme sayfası eklendi api bağlantısı sağlandı model katmanı düzenlendi

---
+++ Foniste/Foniste/Controllers/AddProjectController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Foniste.Models.Ventures;
+
+namespace Foniste.Controllers
+{
+    public class AddProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> AddProject(VenturesAll newVenture)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7173"); // API'nin adresi
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // Yeni projeyi API'ye gönderilir
+                    HttpResponseMessage response = await client.PostAsJsonAsync("/new/ventures", newVenture);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        return Ok(result);
+                    }
+                    else
+                    {
+                        // API'den gelen hata mesajını alır ve gösterir
+                        string error = await response.Content.ReadAsStringAsync();
+                        return BadRequest(error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Foniste/Foniste/Controllers/VısıonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foniste.Controllers
+{
+    public class VısıonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
